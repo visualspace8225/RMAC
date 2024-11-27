2e21b4abdff6a8c795d0d1ada5f92b7bfb069a77
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-11-27 14:53:40 +0530

    port changed

--- server.js
+++ server.js
@@ -441,5 +441,5 @@ app.get('/logout', (req, res) => {
 
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
